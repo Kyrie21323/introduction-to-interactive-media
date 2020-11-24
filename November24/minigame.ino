e9b4ee9802c5c2ebe61ff9caa919f7ce0c148959
Kyrie21323 <70956027+Kyrie21323@users.noreply.github.com>
2020-11-25 02:57:38 +0900

    Create minigame.ino

---
+++ November24/minigame.ino
@@ -0,0 +1,32 @@
+const int ledPin = 6; // the pin that the LED is attached to
+int key = 4;          // pin that the button is attached to
+int incomingByte;      // a variable to read incoming serial data into
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(key, INPUT);
+}
+
+void loop() {
+  // see if there's incoming serial data:
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+    // if it's a capital H (ASCII 72), turn on the LED:
+    if (incomingByte == 'H') {
+      digitalWrite(ledPin, HIGH);
+    }
+    // if it's an L (ASCII 76) turn off the LED:
+    if (incomingByte == 'L') {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+  // send the value of analog input 0:
+  Serial.println(analogRead(A0));
+  // wait a bit for the analog-to-digital converter to stabilize after the last
+  // reading:
+  delay(2);
+}
